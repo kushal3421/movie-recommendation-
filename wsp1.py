33f000da7caed23948ee3c871d8f8dfb0e803364
kushal3421 <121809555+kushal3421@users.noreply.github.com>
2023-08-19 15:34:33 +0530

    Add files via upload

---
+++ wsp1.py
@@ -0,0 +1,8 @@
+import requests
+from bs4 import BeautifulSoup
+url = "https://codewithharry.com"
+r = requests.get(url)
+htmlContent = r.content
+# print(htmlContent)
+soup = BeautifulSoup(htmlContent,'html.parser')
+print(soup.prettify)
